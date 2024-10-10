e4c5b9a836b4331be63b7b49ac30d38aaa52215e
saeedvlt <104569410+saeedvlt@users.noreply.github.com>
2024-10-10 09:45:55 -0400

    Update Daily sales report CONF.py

--- Daily sales report CONF.py
+++ Daily sales report CONF.py
@@ -5,8 +5,8 @@ import streamlit as st
 import pandas as pd
 
 # Example app
-st.title("Single CSV File Processor")
-st.write("This web app processes a sales CSV file.")
+st.title("Conf sales cleaner")
+st.write("This web app processes a sales CSV file for Conf.")
 
 # Upload the CSV file
 uploaded_file = st.file_uploader("Choose the CSV file (media.csv)", type="csv")
