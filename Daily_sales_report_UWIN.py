081da535f17ae0641d1627e470d78fcad14724a2
saeedvlt <104569410+saeedvlt@users.noreply.github.com>
2024-10-10 09:33:34 -0400

    Add files via upload

--- Daily_sales_report_UWIN.py
+++ Daily_sales_report_UWIN.py
@@ -9,6 +9,15 @@ import streamlit as st
 st.title("Daily_sales")
 st.write("Hello, this is daily sales cleaner web app!")
 
+import streamlit as st
+import pandas as pd
+
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file is not None:
+    file1 = pd.read_csv(uploaded_file)
+    st.write(file1)
+
 
 import pandas as pd
 
